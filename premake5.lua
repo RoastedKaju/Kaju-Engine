93f648d3df91b751edbfba11fc198a97c0f248a6
R.Haris Nauman <208430036+RoastedKaju@users.noreply.github.com>
2025-04-22 10:30:03 +0500

    Add Premake file

---
+++ premake5.lua
@@ -0,0 +1,110 @@
+workspace "Kaju"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Distribution"
+	}
+
+	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Kaju Project --
+project "Kaju"
+	location "Kaju"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir("Binaries/" .. outputdir .. "/%{prj.name}")
+	objdir("Intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/Source/**.h",
+		"%{prj.name}/Source/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/ThirdParty/SPD/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		buildoptions { "/utf-8" }
+
+		defines
+		{
+			"KJ_PLATFORM_WINDOWS",
+			"KJ_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../Binaries/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "KJ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "KJ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Distribution"
+		defines "KJ_DIST"
+		optimize "On"
+
+-- Sandbox Project --
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir("Binaries/" .. outputdir .. "/%{prj.name}")
+	objdir("Intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/Source/**.h",
+		"%{prj.name}/Source/**.cpp"
+	}
+
+	includedirs
+	{
+		"Kaju/ThirdParty/SPD/include",
+		"Kaju/Source"
+	}
+
+	links
+	{
+		"Kaju"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		buildoptions { "/utf-8" }
+
+		defines
+		{
+			"KJ_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "KJ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "KJ_RELEASE"
+		optimize "On"
+
+	filter "configurations:Distribution"
+		defines "KJ_DIST"
+		optimize "On"
